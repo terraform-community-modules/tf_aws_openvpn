beb80a674f54b252f61ae11b7dd2c10f9538f852
user <user@workstation.firehawkvfx.com>
2019-01-12 12:31:33 +1000

    added outgoing rule to allow connections to vpc

--- main.tf
+++ main.tf
@@ -55,7 +55,14 @@ resource "aws_security_group" "openvpn" {
     from_port   = 0
     to_port     = 0
     cidr_blocks = ["${var.remote_vpn_ip_cidr}"]
-    description = "all outgoing traffic"
+    description = "all outgoing traffic to vpn client remote ip"
+  }
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["${var.vpc_cidr}"]
+    description = "all outgoing traffic to vpc"
   }
 }
 
