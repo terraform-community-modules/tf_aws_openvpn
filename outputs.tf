a78c7d9eefdef05c5207a2856e440e7b1da96762
Andrew Graham <queglay@gmail.com>
2019-02-25 20:11:18 +1000

    remove outputs attempt.  they break things.

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "id" {
-  value = "${aws_instance.openvpn.id}"
-}
+# output "id" {
+#   value = "${aws_instance.openvpn.id}"
+# }
 
-output "private_ip" {
-  value = "${aws_instance.openvpn.private_ip}"
-}
+# output "private_ip" {
+#   value = "${aws_instance.openvpn.private_ip}"
+# }
 
-output "public_ip" {
-  value = "${aws_eip.openvpnip.public_ip}"
-}
+# output "public_ip" {
+#   value = "${aws_eip.openvpnip.public_ip}"
+# }
 
 # output "public_web_fqdn" {
 #   value = "${aws_route53_record.openvpn-web.fqdn}"
