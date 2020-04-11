751cb18e1d9acba179c8fff1300117d402da5c22
Andrew Graham <queglay@gmail.com>
2020-04-11 11:48:16 +0930

    enable daily service again

--- main.tf
+++ main.tf
@@ -217,7 +217,7 @@ resource "null_resource" "provision_vpn" {
       # "sleep 30", # wait until its started
       # "sudo systemctl start apt-daily.service", # cannot smtop or kill unless its started
       "sudo systemctl disable apt-daily.timer",
-      "sudo systemctl disable apt-daily-upgrade.timer", # the timers way start the daily update, they need to be disabled, but it wont apply until after reboot. stop will also not resolve this.
+      "sudo systemctl disable apt-daily-upgrade.timer", # the timers may start the daily update, they need to be disabled, but it wont apply until after reboot. stop will also not resolve this.
     ]
   }
   provisioner "local-exec" {
@@ -256,6 +256,8 @@ EOT
       "ls /usr/bin",
       "sudo fuser -v /var/cache/debconf/config.dat", # get info if anything else has a lock on this file
       "test=$(which python2.7); if [[ \"$test\" != '/usr/bin/python2.7' ]]; then echo 'failed to use /usr/bin/python2.7'; fi",
+      "sudo systemctl enable apt-daily.timer", # enable again to allow updates
+      "sudo systemctl enable apt-daily-upgrade.timer",
       "echo '...Finished bootstrapping'",
     ]
   }
