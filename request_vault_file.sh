77a826f66d357aa96af21906fc8b45c2a91c5558
Andrew Graham <andrew@firehawkvfx.com>
2021-02-21 14:48:25 +1030

    store files in home dir

--- request_vault_file.sh
+++ request_vault_file.sh
@@ -66,30 +66,35 @@ echo "Aquiring vault data..."
 # data=$(vault kv get -format=json /${resourcetier}/files/usr/local/openvpn_as/scripts/seperate/ca.crt)
 
 function retrieve_file {
-  local -r file_path="$1"
-  # file_path=/usr/local/openvpn_as/scripts/seperate/ca.crt
-  # vault kv get -format=json /${resourcetier}/files/$file_path > /usr/local/openvpn_as/scripts/seperate/ca_test.crt
+  local -r source_path="$1"
+  if [[ -z "$2" ]]; then
+    local -r target_path="$source_path"
+  else
+    local -r target_path="$2"
+  fi
+  # target_path=/usr/local/openvpn_as/scripts/seperate/ca.crt
+  # vault kv get -format=json /${resourcetier}/files/$target_path > /usr/local/openvpn_as/scripts/seperate/ca_test.crt
 
   local -r response=$(retry \
-  "vault kv get -format=json /$resourcetier/files/$file_path" \
+  "vault kv get -format=json /$resourcetier/files/$source_path" \
   "Trying to read secret from vault")
-  sudo mkdir -p $(dirname $file_path) # ensure the directory exists
-  echo $response | jq -r .data.data.file | sudo tee $file_path
+  sudo mkdir -p $(dirname $target_path) # ensure the directory exists
+  echo $response | jq -r .data.data.file | sudo tee $target_path
   local -r permissions=$(echo $response | jq -r .data.data.permissions)
   local -r uid=$(echo $response | jq -r .data.data.uid)
   local -r gid=$(echo $response | jq -r .data.data.gid)
   echo "Setting:"
-  echo "uid:$uid gid:$gid permissions:$permissions file_path:$file_path"
-  sudo chown $uid:$gid $file_path
-  sudo chmod $permissions $file_path
+  echo "uid:$uid gid:$gid permissions:$permissions target_path:$target_path"
+  sudo chown $uid:$gid $target_path
+  sudo chmod $permissions $target_path
 }
 
 # Retrieve previously generated secrets from Vault.  Would be better if we can use vault as an intermediary to generate certs.
 
-retrieve_file "/usr/local/openvpn_as/scripts/seperate/ca.crt"
-retrieve_file "/usr/local/openvpn_as/scripts/seperate/client.crt"
-retrieve_file "/usr/local/openvpn_as/scripts/seperate/client.key"
-retrieve_file "/usr/local/openvpn_as/scripts/seperate/ta.key"
-retrieve_file "/usr/local/openvpn_as/scripts/seperate/client.ovpn"
+retrieve_file "/usr/local/openvpn_as/scripts/seperate/ca.crt" "$HOME/tmp/usr/local/openvpn_as/scripts/seperate/ca.crt"
+retrieve_file "/usr/local/openvpn_as/scripts/seperate/client.crt" "$HOME/tmp/usr/local/openvpn_as/scripts/seperate/client.crt"
+retrieve_file "/usr/local/openvpn_as/scripts/seperate/client.key" "$HOME/tmp/usr/local/openvpn_as/scripts/seperate/client.key"
+retrieve_file "/usr/local/openvpn_as/scripts/seperate/ta.key" "$HOME/tmp/usr/local/openvpn_as/scripts/seperate/ta.key"
+retrieve_file "/usr/local/openvpn_as/scripts/seperate/client.ovpn" "$HOME/tmp/usr/local/openvpn_as/scripts/seperate/client.ovpn"
 
 echo "Done."
