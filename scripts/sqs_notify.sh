43576412d4df80ef8739640ccffd6974088760a8
Andrew Graham <queglay@gmail.com>
2021-06-14 14:13:38 +0930

    update sqs notify heredoc

--- scripts/sqs_notify.sh
+++ scripts/sqs_notify.sh
@@ -11,19 +11,18 @@ readonly sqs_queue_url="$2"
 readonly host1="$3"
 readonly host2="$4"
 
-# readonly VAULT_ADDR="https://vault.service.consul:8200"
 readonly VAULT_ADDR=https://vault.service.consul:8200
 
-openvpn_admin_pw="$(vault kv get -address="$VAULT_ADDR" -format=json $resourcetier/network/openvpn_admin_pw)"
+openvpn_admin_pw="$(vault kv get -field=value -address="$VAULT_ADDR" -format=json $resourcetier/network/openvpn_admin_pw)"
 token="$(vault token create -address="$VAULT_ADDR" -policy=vpn_read_config -policy=deadline_client -explicit-max-ttl=5m -ttl=5m -use-limit=4 -field=token)"
 
-file_content="<< EOF
+file_content=<<EOF
 {
     \"openvpn_admin_pw\" : \"$openvpn_admin_pw\", 
     \"host1\" : \"$host1\",
     \"host2\" : \"$host2\",
     \"token\" : \"$token\"
 }
-EOF"
+EOF
 
 aws sqs send-message --queue-url $sqs_queue_url --message-body "$file_content" --message-group-id "$resourcetier"
